444ecd311edbd165a89b3c629444260b52fccbf4
ImDaClassic <102368409+imdaclassic@users.noreply.github.com>
2025-10-08 17:07:32 +0300

    Update bootloader.py

--- src/bootloader.py
+++ src/bootloader.py
@@ -32,8 +32,13 @@ ConfirmIs = msgb.askyesno("Confirm Position", "Are you sure that this file is in
 if not ConfirmIs:
     sys.exit()
 
-msgb.showwarning("Notice", "Notice: This is NOT an official extention. It does not interact with the LunarV2 program in any way beyond what a normal user can already do. It is simply a helper plugin.")
-msgb.showinfo("Creator", "This program was made fully by @imdaclassic on discord, you are NOT allowed to steal this plugin and rename it for your benefit, this plugin is open source on github for your own peace of mind.")
+
+msgb.showinfo(
+    "Notice & Creator",
+    "⚠️ Notice: This is NOT an official extension. It does not interact with the LunarV2 program in any way beyond what a normal user can already do. It is simply a helper plugin.\n\n"
+    "👤 Creator: This program was made fully by @imdaclassic on Discord. You are NOT allowed to steal this plugin and rename it for your benefit. "
+    "This plugin is open source on GitHub for your own peace of mind."
+)
 
 def fetch_lunarplus_index():
     primary_url = "https://raw.githubusercontent.com/imdaclassic/LunarPlus/main/src/index.json"
@@ -72,6 +77,6 @@ for i in IndexData.get("LunarIndex", []):
     print(f"Lunarv2_Ver: {lunarver} | Get-Link: {rawlink}")
 print("---------")
 
-msgb.showinfo("Development", "Bootloader/plugin still in development.")
+msgb.showwarning("Development", "Bootloader/plugin still in development.")
 
 msgb.showinfo("Success", "LunarPlus Extention has been successfully installed, launch it with either START.bat or LP-Manager.bat (Refresh Files to see LP-Manager). To uninstall/update use LP-Manager.bat | If you encounter any issues, please contact @imdaclassic on discord.")
