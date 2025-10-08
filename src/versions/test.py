a60ffc8d2f01eae67c9936cc4a1c08fc67f37abe
ImDaClassic <102368409+imdaclassic@users.noreply.github.com>
2025-10-08 16:56:49 +0300

    Update test.py

--- src/versions/test.py
+++ src/versions/test.py
@@ -1,6 +1,2 @@
-import os
-def clr():
-    os.system('cls' if os.name == 'nt' else 'clear')
-clr()
-print("Running directory: "os.getcwd())
-input("Test program running.")
+from tkinter import messagebox as msgb
+msgb.showinfo("Development", "Extention still in development.")
